15e00502e37b224bfd15fc4f85981aefde164122
grasword <grasword6@gmail.com>
2023-01-29 15:44:12 +0000

    added base URL for cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  watchForFileChanges: false,
   e2e: {
+    baseUrl: 'http://localhost:3000',
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
-    },
-  },
+    }
+  }
 })
