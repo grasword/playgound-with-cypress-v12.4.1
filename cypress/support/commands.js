4ef81d72a0a292d0ea2e59c936115b96c8bf1d1e
grasword <grasword6@gmail.com>
2023-01-29 16:08:55 +0000

    refactored commands logic to wrap the response body

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,9 +25,13 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('getAllPosts', () => {
-  return cy.request('/api/posts')
+  cy.request('/api/posts').then((res) => {
+    return cy.wrap(res.body)
+  })
 })
 
 Cypress.Commands.add('getFirstPost', () => {
-  return cy.request('/api/posts/1')
+  cy.request('/api/posts').then((res) => {
+    return cy.wrap(res.body).its(0)
+  })
 })
